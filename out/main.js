0fef63ed2b53db25dd102a2379a70692a73087b0
jacl38 <clarkjack1138@gmail.com>
2023-02-10 19:55:10 -0500

    Fixed AddRandomMatches for real this time

--- out/main.js
+++ out/main.js
@@ -326,37 +326,73 @@ const ConfirmSubmit = () => {
 };
 
 const AddRandomMatches = count => {
-	const teams = [...Array(40)].map(value => Math.floor(Math.random() * 10000));
-	for(let i = 0; i < count; i++) {
-		const matchTeams = teams.sort(() => 0.5 - Math.random()).slice(0, 3);
-		let remainingCells = 9 * 3;
-		const RandomTeamData = number => {
-			const scoreCount = Math.floor(Math.random() * remainingCells);
-			remainingCells -= scoreCount;
-			const RandomRowData = [...Array(3)].map(row => [...Array(9)].map(value => 0));
-			for(let c = 0; c < scoreCount; c++) {
-				let cellPosition = { row: Math.floor(Math.random() * 3), cell: Math.floor(Math.random() * 9) };
-				while(RandomRowData[cellPosition.row][cellPosition.cell] != 0) {
-					cellPosition = { row: Math.floor(Math.random() * 3), cell: Math.floor(Math.random() * 9) };
-				}
-				RandomRowData[cellPosition.row][cellPosition.cell] = Math.floor(Math.random() * 3) * (Math.random() > 0.5 ? -1 : 1);
-			}
-			return {
-				teamNumber: number,
-				autonomous: { mobility: Math.random() > 0.5, charge: Math.floor(Math.random() * 3) },
-				endgame: { charge: Math.floor(Math.random() * 3) },
-				notes: "Randomly generated data",
-				scoreMatrix: { high: RandomRowData[0], mid: RandomRowData[1], low: RandomRowData[2] }
-			}
-		};
-		const result = {
-			submitTime: 0,
-			matchNumber: i + 1,
-			alliance: Math.random() > 0.5? "red" : "blue",
-			win: Math.random() > 0.5,
-			teamData: [...Array(3)].map((team, index) => RandomTeamData(matchTeams[index]))
+	const teams = [];
+
+	[...Array(40)].map(_ => {
+		let newTeam = Math.floor(Math.random()**1.5 * 9999) + 1;
+		while(teams.includes(newTeam)) {
+			newTeam = Math.floor(Math.random()**1.5 * 9999) + 1;
 		}
-		AddSubmission(result);
+		teams.push(newTeam);
+	});
+
+	const emptyMatrix = () => {
+		return {
+			"low": [...Array(9)].map(_ => 0),
+			"mid": [...Array(9)].map(_ => 0),
+			"high": [...Array(9)].map(_ => 0)
+		};
+	}
+
+	for(let i = 0; i < count; i++) {
+		const shuffledTeams = teams.sort(() => 0.5 - Math.random());
+		const allianceTeams = {
+			"red": shuffledTeams.slice(0, 3),
+			"blue": shuffledTeams.slice(3, 6)
+		};
+		const remainingCells = {
+			"red": Math.ceil((1-Math.random()**2) * 27),
+			"blue": Math.ceil((1-Math.random()**2) * 27)
+		};
+		const filledRows = {
+			"red": emptyMatrix(),
+			"blue": emptyMatrix()
+		}
+		const winAlliance = ["red", "blue"][Math.floor(Math.random() * 2)];
+
+		["red", "blue"].forEach(alliance => {
+			const selectedTeams = [];
+			allianceTeams[alliance].forEach(team => {
+				const ThisTeamRows = emptyMatrix();
+				const scoreCount = Math.ceil(Math.random() * remainingCells[alliance]);
+				remainingCells[alliance] -= scoreCount;
+
+				for(let c = 0; c < scoreCount; c++) {
+					let cellPosition = { row: rowNames[Math.floor(Math.random() * 3)], cell: Math.floor(Math.random() * 9) };
+					while(filledRows[alliance][cellPosition.row][cellPosition.cell] != 0) {
+						cellPosition = { row: rowNames[Math.floor(Math.random() * 3)], cell: Math.floor(Math.random() * 9) };
+					}
+					const cubeOrCone = cellPosition.row == "low" ? Math.floor(Math.random() * 2) + 1 : Math.abs(cellPosition.cell % 3 - 1) + 1;
+					filledRows[alliance][cellPosition.row][cellPosition.cell] = cubeOrCone * (Math.random() > 0.5 ? -1 : 1);
+					ThisTeamRows[cellPosition.row][cellPosition.cell] = filledRows[alliance][cellPosition.row][cellPosition.cell];
+				}
+				selectedTeams.push({
+					teamNumber: team,
+					autonomous: { mobility: Math.random() > 0.5, charge: Math.floor(Math.random() * 3) },
+					endgame: { charge: Math.floor(Math.random() * 3) },
+					notes: "Randomly generated data",
+					scoreMatrix: ThisTeamRows
+				})
+			});
+
+			AddSubmission({
+				submitTime: 0,
+				matchNumber: i + 1,
+				alliance: alliance,
+				win: winAlliance == alliance,
+				teamData: selectedTeams
+			});
+		});
 	}
 };
 
